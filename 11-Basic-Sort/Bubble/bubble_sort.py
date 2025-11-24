28d7a92780fd74a2f654b24a53c8110ef83f0b26
Francesco <hart.francesco@gmail.com>
2025-11-24 12:58:42 +0100

    bubble

--- 11-Basic-Sort/Bubble/bubble_sort.py
+++ 11-Basic-Sort/Bubble/bubble_sort.py
@@ -1,19 +1,10 @@
-
-
-def bubble_sort(l):
-    for i in range(len(l) - 1, 0, -1):
-        j = 0
-        while j < i:
-            value = l[j]
-            if value > l[j + 1]:
-                temp = l[j + 1]
-                l[j+1] = value
-                l[j] = temp
-            j += 1
-            
-    return l
-
-            
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n - 1):
+        for j in range(0, n - 1 - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
 
 
 print(bubble_sort([4,2,6,5,1,3]))
